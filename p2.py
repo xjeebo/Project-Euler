b6e493b6a2e3603f36dc9b986d5690faa3fe86fe
Jeeno <35685060+xjeebo@users.noreply.github.com>
2018-02-26 09:35:31 -0800

    Fibonacci sequence even sum completed

--- p2.py
+++ p2.py
@@ -4,6 +4,7 @@
 # By considering the terms in the Fibonacci sequence whose values do not exceed four million,
 # find the sum of the even-valued terms.
 
+# Another method is by recursion (this isn't by recursion)
 i = 0
 j = 1
 sum = 0
