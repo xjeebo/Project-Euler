9f65980c7f4a8f34cff896cfaafd7ad14eba9b15
Jeeno <35685060+xjeebo@users.noreply.github.com>
2018-03-02 15:02:09 -0800

    difference between the square sum and sum of squares

---
+++ p6.py
@@ -0,0 +1,16 @@
+# The sum of the squares of the first ten natural numbers is,
+# 1^2 + 2^2 + ... + 10^2 = 385
+# The square of the sum of the first ten natural numbers is,
+# (1 + 2 + ... + 10)2 = 552 = 3025
+# Hence the difference between the sum of the squares of the first ten natural numbers
+# and the square of the sum is 3025 − 385 = 2640.
+# Find the difference between the sum of the squares of the first one hundred natural
+# numbers and the square of the sum.
+
+SumSquare = 0
+SquareSum = 0
+for i in range(1,101):
+    SumSquare += i**2
+    SquareSum += i
+
+print("Answer: "+str(SquareSum**2-SumSquare))
