068eb4bf98e721bdfb3dfe97260ba193382d2ac3
Jeeno <35685060+xjeebo@users.noreply.github.com>
2018-03-04 16:25:56 -0800

    added comments
    the smallest positive number that is evenly divisible by all of the numbers from 1 to 20

--- p5.py
+++ p5.py
@@ -2,21 +2,21 @@
 # What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
 
 smallest = 0
-num = 1
+num = 1      # number that is checked for divisibility
 flag = False
 while 1:
 
-    for i in range(1,21):
-        if num % i != 0:
-            flag = False
+    for i in range(1,21): # check from 1 - 20
+        if num % i != 0:  # check if the val is not divisible from any num from 1 - 20
+            flag = False  # exit loop right away if so
             break
         elif num % i == 0:
             flag = True
-    if flag == True:
+    if flag == True:      # if it never terminates early, then the smallest is found
         smallest = num
         break
             
-    num += 1
+    num += 1 # check next value
 
 print(smallest)
 
