f57d1003d3405c52cee98e8f513c4816feca9f48
Jeeno <35685060+xjeebo@users.noreply.github.com>
2018-03-04 16:28:37 -0800

    additional comments
    the largest prime factor of the number 600851475143?

--- p3.py
+++ p3.py
@@ -1,12 +1,12 @@
 # The prime factors of 13195 are 5, 7, 13 and 29.
 # What is the largest prime factor of the number 600851475143?
 
-n = 600851475143
+n = 600851475143 # given value
 i = 2
-while i < n:
+while i < n:    
 
-    if n % i == 0:
-        n = n/i
-        i = 2
-    i += 1
+    if n % i == 0:  # check if the given value is divisible by i, which is incremented every time by 1
+        n = n/i     # if it is divisible, divide num by that val and reset the num
+        i = 2       # reset i
+    i += 1          # when I becomes = to n then the value had only been divisible by 1 and itself, which means it is prime
 print(n) # the largest prime number
